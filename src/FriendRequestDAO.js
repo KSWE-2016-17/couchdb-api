561c7484e69ace31303db65003c4cab4dded39f2
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-11-18 09:08:58 +0100

    Use GraphQL queries and mutations in FriendRequestDAO

---
+++ src/FriendRequestDAO.js
@@ -0,0 +1,60 @@
+var DaoHelper = require("./DaoHelper")
+
+var FriendRequestDAO = function(connection) {
+    this.connection = connection;
+    this.daoHelper = new DaoHelper();
+};
+
+FriendRequestDAO.prototype.findAll = function() {
+    return this.daoHelper.find({
+        query: "{ friendRequests { _id from_id to_id } }"
+    }, this.connection.getFullUrl());
+};
+
+FriendRequestDAO.prototype.findById = function(id) {
+    return this.daoHelper.find({
+        query: "{ friendRequest(_id: '" + id + "') { _id from_id to_id } }"
+    }, this.connection.getFullUrl());
+};
+
+FriendRequestDAO.prototype.findByFrom = function(id) {
+    return this.daoHelper.find({
+        query: "{ friendRequests(filter: { from_id: '" + id + "' }) { _id from_id to_id } }"
+    }, this.connection.getFullUrl());
+};
+
+FriendRequestDAO.prototype.findByTo = function(id) {
+    return this.daoHelper.find({
+        query: "{ friendRequests(filter: { to_id: '" + id + "' }) { _id from_id to_id } }"
+    }, this.connection.getFullUrl());
+};
+
+FriendRequestDAO.prototype.create = function(obj) {
+    return this.daoHelper.create({
+        query: "mutation { createFriendRequest(record: { from_id: '" + obj.from_id + "', to_id: '" + obj.to_id + "' }) { record { _id from_id to_id } } }"
+    }, this.connection.getFullUrl());
+};
+
+FriendRequestDAO.prototype.update = function(obj) {
+    return this.daoHelper.update({
+        query: "mutation { updateFriendRequest(record: { _id: '" + obj._id + "', from_id: '" + obj.from_id + "', to_id: '" + obj.to_id + "' }) { record { _id from_id to_id } } }"
+    }, this.connection.getFullUrl());
+};
+
+FriendRequestDAO.prototype.createOrUpdate = function(obj) {
+    if (obj._id) {
+        return this.update(obj);
+    } else {
+        return this.create(obj);
+    }
+};
+
+FriendRequestDAO.prototype.delete = function(obj) {
+    return this.daoHelper.delete({
+        query: "{ removeFriendRequest(_id: '" + obj._id + "') { _id from_id to_id } }"
+    }, this.connection.getFullUrl());
+};
+
+exports.default = FriendRequestDAO;
+module.exports = exports.default;
+
