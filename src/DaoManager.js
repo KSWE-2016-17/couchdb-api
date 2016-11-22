16d857ab27e82270a1211349637a098427d160d0
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-11-22 15:20:57 +0100

    Clean up connection properties

--- src/DaoManager.js
+++ src/DaoManager.js
@@ -1,49 +1,17 @@
 var DaoManager = function(connectionProperties) {
     this._daos = {};
     this.connection = {
-        protocol: "https",
-        user: "",
-        password: "",
-        url: "", // Mandatory
-        port: "",
-        database: "" // Mandatory
+        url: "" // Mandatory
     };
     this.connection.getFullUrl = function() {
-        var result = "";
-
-        result += this.protocol ? this.protocol + "://" : "https://";
-        result += this.user ? this.user : "";
-        result += this.user && this.password ? ":" + this.password : "";
-        result += this.user ? "@" : "";
-        result += this.url;
-        result += this.port ? ":" + this.port : "";
-        result += "/" + this.database + "/";
-
-        return result;
+        return this.url;
     };
 
     if (connectionProperties) {
-        if (connectionProperties.protocol) {
-            this.connection.protocol = connectionProperties.protocol;
-        }
-        if (connectionProperties.user) {
-            this.connection.user = connectionProperties.user;
-        }
-        if (connectionProperties.password) {
-            this.connection.password = connectionProperties.password;
-        }
         if (connectionProperties.url) {
             this.connection.url = connectionProperties.url;
         } else {
             console.error("No URL given!")
-        }
-        if (connectionProperties.port) {
-            this.connection.port = connectionProperties.port;
-        }
-        if (connectionProperties.database) {
-            this.connection.database = connectionProperties.database;
-        } else {
-            console.error("No database given!");
         }
     } else {
         console.error("No connection properties given!");
